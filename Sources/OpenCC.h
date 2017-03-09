5d4462ba78e53539514f800516bdfb6542459263
Xander Deng <dengxiang2010@gmail.com>
2017-03-09 13:19:04 +0800

    Add: Targets

---
+++ Sources/OpenCC.h
@@ -0,0 +1,19 @@
+//
+//  OpenCC.h
+//  OpenCC
+//
+//  Created by 邓翔 on 2017/3/9.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OpenCC.
+FOUNDATION_EXPORT double OpenCCVersionNumber;
+
+//! Project version string for OpenCC.
+FOUNDATION_EXPORT const unsigned char OpenCCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenCC/PublicHeader.h>
+
+
