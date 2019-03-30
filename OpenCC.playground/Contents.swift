5750fd25c9163d2ae664a48e857bad06678bf31c
Xander Deng <dengxiang2010@gmail.com>
2019-03-30 12:51:06 +0800

    update playground

--- OpenCC.playground/Contents.swift
+++ OpenCC.playground/Contents.swift
@@ -3,6 +3,6 @@ import OpenCC
 
 let str = "鼠标里面的硅二极管坏了，导致光标分辨率降低。"
 
-let converter = ChineseConverter(option: [.traditionalize, .TWStandard, .TWIdiom])
+let converter = try! ChineseConverter(option: [.traditionalize, .TWStandard, .TWIdiom])
 
 print(converter.convert(str))
